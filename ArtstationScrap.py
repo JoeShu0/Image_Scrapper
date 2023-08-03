06c4e2b1094750a0e108d75894fc75f06b09816d
JoeShu0 <sccdsix@163.com>
2023-08-03 16:20:46 +0800

    Update ArtstationScrap.py

--- ArtstationScrap.py
+++ ArtstationScrap.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import re
 import os
+import traceback
 
 MainFolder = "Artstation"
 
@@ -36,99 +37,107 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
     'cookie': '__stripe_mid=01bf723f-ae7b-409f-895d-e484e37e48540ff168; visitor-uuid=ed425b76-d521-4569-bbc7-f3bf4c6f5e6f; __cf_bm=OtyoUq3RnM2V1vT87MrDspqOCA01MfVVKYKxr3CLWSQ-1688311217-0-AdEbRsiqoiYXK8/G596N2/hJy9tvfJrNz1lgldiXF837EmKya8AXl23BGWWtNPNwTFG4cPxbbgExMECt66ghUHumCcAkVUib2+5Av4SQniL0twqPZZq7+VzYFiMIGZEMYw==; PRIVATE-CSRF-TOKEN=ZBXBTa%2BWzJikLxdXIgO28iVnTcM1oey0PqhalO%2BPYTY%3D; __stripe_sid=8a993040-139a-440d-8577-85e2cb72da63f57c07',
 }
-s=input('输入的关键词: ')
-ssd=int(input('输要爬的个数: '))
-page=int(ssd//50)
-ssd=int(ssd%50)
-if page>0:
-    for x in range(1,page+1):
-        json_data = {
-            'query': s,
-            'page': x,
-            'per_page': 50,
-            'sorting': 'relevance',
-            'pro_first': '1',
-            'filters': [],
-            'additional_fields': [],
-        }
-        response = requests.post(
-            'https://www.artstation.com/api/v2/search/projects.json',
-            headers=headers,
-            json=json_data,
-        ).json()['data']
-        for u,i in enumerate(response):
-            if x==page:
-                if u <= ssd:
-                    a1=i['title']+'-'+i['hash_id']
-                    a2=get_url(i['hash_id'])
-                    a3=i['hash_id']
-                    for x,r in enumerate (a2):
-                        path=validateTitle(a1)
-                        try:
-                            os.makedirs(f'Output\\Artstation\\{path}')
-                        except:
-                            path=validateTitle(a3)
-                            os.makedirs(f'Output\\Artstation\\{path}')
-                        if x==1:
-                            pass
-                        else:
-                            name=str(r).split('/')[-1].split('?')[0]
-                            with open(f'Output\\Artstation\\{path}\{name}','wb')as a:
-                                a.write(requests.get(r).content)
-                            print(f'{path}-{name}写入完成')
-            else:
-                a1=i['title']+'-'+i['hash_id']
-                a2=get_url(i['hash_id'])
-                a3=i['hash_id']
-                
-                for x,r in enumerate (a2):
-                    path=str(validateTitle(a1)).replace(' ','')[:127]
-                    try:
-                        os.makedirs(f'Output\\Artstation\\{path}')
-                    except:
-                        pass
-                    if x==1:
-                        pass
-                    else:
-                        name=str(r).split('/')[-1].split('?')[0]
-                        with open(f'Output\\Artstation\\{path}\{name}','wb')as a:
-                            a.write(requests.get(r).content)
-                        print(f'{path}-{name}写入完成')
 
-else:
-    for x in range(1,2):
-        json_data = {
-            'query': s,
-            'page': x,
-            'per_page': 50,
-            'sorting': 'relevance',
-            'pro_first': '1',
-            'filters': [],
-            'additional_fields': [],
-        }
-        response = requests.post(
-            'https://www.artstation.com/api/v2/search/projects.json',
-            headers=headers,
-            json=json_data,
-        ).json()['data']
-        for u,i in enumerate(response):
-                if u <= ssd:
-                    a1=i['title']+'-'+i['hash_id']
-                    a2=get_url(i['hash_id'])
-                    a3=i['hash_id']
-                    print(a2)
-                    for x,r in enumerate (a2):
-                        path=str(validateTitle(a1)).replace(' ','')[:127]
-                        try:
-                            os.makedirs(f'Output\\Artstation\\{path}')
-                        except:
-                            pass
-                        if x==1:
-                            pass
-                        else:
-                            name=str(r).split('/')[-1].split('?')[0]
-                            with open(f'Output\\Artstation\\{path}\{name}','wb')as a:
-                                a.write(requests.get(r).content)
-                            print(f'{path}-{name}写入完成')
+if __name__ == "__main__":
+    s=input('输入的关键词: ')
+    ssd=int(input('输要爬的个数: '))
+    page=int(ssd//50)
+    ssd=int(ssd%50)
+
+    try:
+        if page>0:
+            for x in range(1,page+1):
+                json_data = {
+                    'query': s,
+                    'page': x,
+                    'per_page': 50,
+                    'sorting': 'relevance',
+                    'pro_first': '1',
+                    'filters': [],
+                    'additional_fields': [],
+                }
+                response = requests.post(
+                    'https://www.artstation.com/api/v2/search/projects.json',
+                    headers=headers,
+                    json=json_data,
+                ).json()['data']
+                for u,i in enumerate(response):
+                    if x==page:
+                        if u <= ssd:
+                            a1=i['title']+'-'+i['hash_id']
+                            a2=get_url(i['hash_id'])
+                            a3=i['hash_id']
+                            for x,r in enumerate (a2):
+                                path=validateTitle(a1)
+                                try:
+                                    os.makedirs(f'Output\\Artstation\\{path}', exist_ok=True)
+                                except:
+                                    path=validateTitle(a3)
+                                    os.makedirs(f'Output\\Artstation\\{path}', exist_ok=True)
+                                if x==1:
+                                    pass
+                                else:
+                                    name=str(r).split('/')[-1].split('?')[0]
+                                    with open(f'Output\\Artstation\\{path}\{name}','wb')as a:
+                                        a.write(requests.get(r).content)
+                                    print(f'{path}-{name}写入完成')
+                    else:
+                        a1=i['title']+'-'+i['hash_id']
+                        a2=get_url(i['hash_id'])
+                        a3=i['hash_id']
+                        
+                        for x,r in enumerate (a2):
+                            path=str(validateTitle(a1)).replace(' ','')[:127]
+                            try:
+                                os.makedirs(f'Output\\Artstation\\{path}', exist_ok=True)
+                            except:
+                                pass
+                            if x==1:
+                                pass
+                            else:
+                                name=str(r).split('/')[-1].split('?')[0]
+                                with open(f'Output\\Artstation\\{path}\{name}','wb')as a:
+                                    a.write(requests.get(r).content)
+                                print(f'{path}-{name}写入完成')
+
+        else:
+            for x in range(1,2):
+                json_data = {
+                    'query': s,
+                    'page': x,
+                    'per_page': 50,
+                    'sorting': 'relevance',
+                    'pro_first': '1',
+                    'filters': [],
+                    'additional_fields': [],
+                }
+                response = requests.post(
+                    'https://www.artstation.com/api/v2/search/projects.json',
+                    headers=headers,
+                    json=json_data,
+                ).json()['data']
+                for u,i in enumerate(response):
+                        if u <= ssd:
+                            a1=i['title']+'-'+i['hash_id']
+                            a2=get_url(i['hash_id'])
+                            a3=i['hash_id']
+                            print(a2)
+                            for x,r in enumerate (a2):
+                                path=str(validateTitle(a1)).replace(' ','')[:127]
+                                try:
+                                    os.makedirs(f'Output\\Artstation\\{path}', exist_ok=True)
+                                except:
+                                    pass
+                                if x==1:
+                                    pass
+                                else:
+                                    name=str(r).split('/')[-1].split('?')[0]
+                                    with open(f'Output\\Artstation\\{path}\{name}','wb')as a:
+                                        a.write(requests.get(r).content)
+                                    print(f'{path}-{name}写入完成')
+    except Exception:
+        traceback.print_exc()
+        choice = input("脚本出现以上报错，请联系作者或自行修复:(")
+
 
 
